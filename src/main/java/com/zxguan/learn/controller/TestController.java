e699ce50e9846474fdd42745077c8f5e2afcde9a
zxguan <zxguan_star@163.com>
2018-08-01 17:42:04 +0800

    v1

---
+++ src/main/java/com/zxguan/learn/controller/TestController.java
@@ -0,0 +1,8 @@
+package com.zxguan.learn.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TestController {
+
+}
---
+++ src/main/java/com/zxguan/learn/model/Book.java
@@ -0,0 +1,51 @@
+package com.zxguan.learn.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="book")
+public class Book implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name="book_name")
+    private String bookName;
+
+    @Column(name="book_author")
+    private String bookAuthor;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getBookAuthor() {
+        return bookAuthor;
+    }
+
+    public void setBookAuthor(String bookAuthor) {
+        this.bookAuthor = bookAuthor;
+    }
+}
